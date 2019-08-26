5f9fc986bc5ee55bae8a075930e7c0c630e23074
Daniel <mwdchang@gmail.com>
2019-08-25 23:38:59 -0400

    update

---
+++ lorenz.js
@@ -0,0 +1,24 @@
+class Lorenz {
+  constructor() {
+    this.x = 0.1; 
+    this.y = 0;
+    this.z = 0; 
+    this.a = 10.0;
+    this.b = 28.0;
+    this.c = 8.0 / 3.0;
+  }
+
+  getPosition() {
+    return { x: this.x, y: this.y, z: this.z };
+  }
+
+  next() {
+    const t = 0.01; 
+    const xt = t * this.a * (this.y - this.x);
+    const yt = t * (this.x * (this.b - this.z) - this.y);
+    const zt = t * (this.x * this.y - this.c * this.z);
+    this.x = this.x + xt;
+    this.y = this.y + yt;
+    this.z = this.z + zt;
+  }
+}
